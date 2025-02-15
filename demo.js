828c777b1c63b8362cab84bdabd02abc2384e310
Nitr0Skay <cin.miko94@gmail.com>
2025-02-15 20:26:01 +0100

    Add removing warning class

--- demo.js
+++ demo.js
@@ -11,6 +11,9 @@ function updateRemainingCharacters(event) {
     if(remainingCharacters <= 10) {
         remainingCharsElement.classList.add('warning');
         productNameInputElement.classList.add('warning');
+    } else {
+        remainingCharsElement.classList.remove('warning');
+        productNameInputElement.classList.remove('warning');
     }
 }
 
