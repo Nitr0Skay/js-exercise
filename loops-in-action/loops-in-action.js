f9106a3e12e607efafe606dac3a458a67f10ba9e
Nitr0Skay <cin.miko94@gmail.com>
2025-02-17 22:39:45 +0100

    for loop

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -1,5 +1,20 @@
 // First example: Sum nubers
 
-const calculateSumButtonElement = document.querySelectro('#calculator buttom');
+const calculateSumButtonElement = document.querySelector('#calculator button');
+
+function calculateSum() {
+    const userNumberInputElement = document.getElementById('user-number');
+    const enteredNumber = userNumberInputElement.value;
+    const outputResultElement = document.getElementById('calculated-sum');
+
+    let sumUpToNumber = 0;
+
+    for(let i = 0; i <= enteredNumber; i++) {
+        sumUpToNumber = sumUpToNumber + i; 
+    }
+    
+    outputResultElement.textContent = sumUpToNumber;
+    outputResultElement.style.display = 'block';
+}
 
 calculateSumButtonElement.addEventListener('click', calculateSum);
